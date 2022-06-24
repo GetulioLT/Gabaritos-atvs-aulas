e9191aea814357ac11bbbd6067aaef963ee5aac8
GetulioLT <88693489+GetulioLT@users.noreply.github.com>
2022-06-24 14:54:54 -0300

    Add files via upload

---
+++ Gabaritos atvs - Domingo/26-06-2022/atividade 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos descobrir que tipo de triangulo se forma com tres retas?");
+
+            Trian trian = new Trian();
+
+            Console.WriteLine("Priemiro preciso que digite as tres retas");
+            Console.WriteLine("Digite o primeiro valor:");
+            trian.l1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digete o segundo valor:");
+            trian.l2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("E finalizando o ultimo valor:");
+            trian.l3 = float.Parse(Console.ReadLine());
+
+            trian.menNegacao();
+            trian.equi();
+            trian.isoc();
+            trian.esca();
+
+
+            trian.mensagem();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Gabaritos atvs - Domingo/26-06-2022/atividade 1/Trian.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+class Trian
+{
+
+    public float l1, l2, l3;
+    private bool neg, eq, iso, es;
+
+
+    public void menNegacao()
+    {
+        if (l1 + l2 < l3 || l2 + l3 < l1 || l1 + l3 < l2)
+        {
+            neg = true;
+            
+        }
+    }
+
+    public void equi()
+    {
+        if (l1 == l2 && l2 == l3 && l1 == l3)
+        {
+            eq = true;
+        }
+        
+    }
+
+    public void isoc()
+    {
+        if (l1 == l2 || l2 == l3 || l1 == l3 && eq == false)
+        {
+            iso = true;
+        }
+    }
+
+    public void esca()
+    {
+        if (l1 != l2 && l2 != l3 && l1 != l3)
+        {
+            es = true;
+        }
+    }
+
+    public void mensagem()
+    {
+
+        if (neg == true)
+        {
+            Console.WriteLine("As tres medidas não formam um triangulo.");
+        }
+        else if (eq == true)
+        {
+            Console.WriteLine($"As medidas: {l1}, {l2} e {l3}, equivalem a um trinagulo equilátero.");
+        }
+        else if (iso == true)
+        {
+            Console.WriteLine($"As medidas: {l1}, {l2} e {l3}, equivalem a um trinagulo isósceles.");
+        }
+        else if (es == true)
+        {
+            Console.WriteLine($"As medidas: {l1}, {l2} e {l3}, equivalem a um trinagulo escaleno.");
+        }
+
+    }
+
+}
