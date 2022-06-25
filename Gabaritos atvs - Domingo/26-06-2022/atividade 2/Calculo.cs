f20a64f1efb482bf98710f680aabfc7a3e1eebfa
GetulioLT <88693489+GetulioLT@users.noreply.github.com>
2022-06-25 13:00:17 -0300

    Add files via upload

---
+++ Gabaritos atvs - Domingo/26-06-2022/atividade 2/Calculo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+class Calculo
+{
+
+    public float a, b, c;
+    public double x0, x1;
+    public float deltA;
+    public bool dM0, d0, d1;
+
+
+    public float delta()
+    {
+        Calculo calculo = new Calculo();
+        deltA = (b * b) - 4 * a * c;
+        condD();
+        return deltA;
+    }
+
+    private void condD()
+    {
+        if (deltA < 0)
+        {
+            dM0 = true;
+        }
+        else if (deltA == 0)
+        {
+            d0 = true;
+            calculoX0();
+        }
+        else
+        {
+            d1 = true;
+            calculoX1();
+        }
+    }
+    
+    private void calculoX0()
+    {
+        x0 = (b * -1 + Math.Sqrt(deltA)) / (2 * a);
+
+    }
+
+    private void calculoX1()
+    {
+        x0 = (b * -1 + Math.Sqrt(deltA)) / (2 * a);
+        x1 = (b * -1 -  Math.Sqrt(deltA)) / (2 * a);
+
+
+    }
+
+
+    public void mensagem()
+    {
+        Console.WriteLine();
+
+        if (d1 == true)
+        {
+            Console.WriteLine($"{a}x² + {b}x + {c}");
+            Console.WriteLine($"D = {b}² * 4 * {a} * {c}\n");
+
+            Console.WriteLine($"Como o delta ficou igual a {deltA} o resultado terá duas raizes.");
+
+            Console.WriteLine($"x = -({b}) ± √D\n" +
+                              $"     2 * {a}\n");
+
+            Console.WriteLine($"calculo ficou com o resultado para x1: {x0.ToString("0.00")} e x2: {x1.ToString("0.00")}.");
+        }
+        else if(d0 == true)
+        {
+            Console.WriteLine($"{a}x² + {b}x + {c}");
+            Console.WriteLine($"D = {b}² * 4 * {a} * {c}\n");
+
+            Console.WriteLine($"Como o delta ficou igual a {deltA} o resultado terá apenas uma raiz.");
+
+            Console.WriteLine($"x = -({b}) ± √D\n" +
+                              $"     2 * {a}\n");
+
+            Console.WriteLine($"calculo ficou com o resultado para x1: {x0.ToString("0.00")}.");
+        }
+        else if(dM0 == true)
+        {
+            Console.WriteLine($"{a}x² + {b}x + {c}");
+            Console.WriteLine($"D = {b}² * 4 * {a} * {c}");
+
+            Console.WriteLine($"Como o delta ficou igual a {deltA} terá resultado com raizes reais");
+        }
+        
+    }
+
+}
---
+++ Gabaritos atvs - Domingo/26-06-2022/atividade 2/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+    Faça um programa que calcule as raízes de uma equação do segundo grau, na forma ax2 + bx + c. 
+    O programa deverá pedir os valores de a, b e c e fazer as consistências, informando ao usuário 
+    nas seguintes situações:  
+    a. Se o usuário informar o valor de A igual a zero. A equação não é do segundo grau e o programa 
+    deve solicitar um novo valor para A;
+    b. Se o delta calculado for negativo, a equação não possui raízes reais. Informe ao usuário e 
+    encerre o programa;
+    c. Se o delta calculado for igual a zero a equação possui apenas uma raiz real; informe ao usuário;
+    d. Se o delta for positivo, a equação possui duas raízes reais; informe-as ao usuário
+
+    Crie uma classe para executar os cálculos e um método para cada cálculo.
+    Use a classe Math e o método Sqrt() para encontrar a raiz quadrada. Math.Sqrt(25) = 5.
+
+*/
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Que tal fazemos um programa para fazer um calculo com um " +
+                "equação do 2 grau?");
+
+            Calculo calculo = new Calculo();
+
+            Console.WriteLine("Prmeiramente nós passe os 3 valores referentes as icognitas a, b e c" +
+                "\nDigite o valor de a:");
+            err1:
+            calculo.a = float.Parse(Console.ReadLine());
+
+            if (calculo.a == 0)
+            {
+                Console.WriteLine("a não pode ser igual a 0 digite Novamente:");
+                goto err1;
+            }
+            Console.WriteLine("Digite b:");
+            calculo.b = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite c:");
+            calculo.c = float.Parse(Console.ReadLine());
+
+            calculo.delta();
+            
+
+            
+
+
+            calculo.mensagem();
+
+            Console.ReadLine();
+        }
+    }
+}
