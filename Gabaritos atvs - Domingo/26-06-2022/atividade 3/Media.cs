df6bfb2e4ef0782cfc0d5fb6251e74c09229ff66
GetulioLT <88693489+GetulioLT@users.noreply.github.com>
2022-06-25 13:55:47 -0300

    Add files via upload

---
+++ Gabaritos atvs - Domingo/26-06-2022/atividade 3/Media.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class Media
+{
+    /*================ Váriaveis ================*/
+
+    public string name;
+    private float n1, n2, n3,medi, p1, p2, p3;
+
+    /*===========================================*/
+
+    public void mediaA()
+    {
+        /*============ Entrada de Dados =============*/
+
+        Console.WriteLine("\nPara media aritmética temos que receber 2 notas sua:");
+        Console.WriteLine("\ndigite nota 1");
+        n1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("digite nota 2");
+        n2 = float.Parse(Console.ReadLine());
+
+        /*===========================================*/
+
+        /*========= Processamento de Dados ==========*/
+
+        medi = (n1 + n2) / 2;
+
+        /*===========================================*/
+
+    }
+
+    public void mediaP()
+    {
+        /*============ Entrada de Dados =============*/
+
+        Console.WriteLine("\nPara media ponderada temos que receber 3 notas e seus respectivos pesos:");
+        Console.WriteLine("\ndigite nota 1");
+        n1 = float.Parse(Console.ReadLine());
+        Console.WriteLine("digite peso 1");
+        p1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("digite nota 2");
+        n2 = float.Parse(Console.ReadLine());
+        Console.WriteLine("digite peso 2");
+        p2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("digite nota 3");
+        n3 = float.Parse(Console.ReadLine());
+        Console.WriteLine("digite peso 3");
+        p3 = float.Parse(Console.ReadLine());
+
+        /*===========================================*/
+
+        /*========= Processamento de Dados ==========*/
+
+        medi = (p1 * n1 + p2 * n2 + p3 * n3) / (p1 + p2 + p3);
+
+        /*===========================================*/
+
+    }
+
+    /*============= Saída de Dados ==============*/
+
+    public void mensagem()
+    {
+        Console.WriteLine($"\n{name} sua média é: {medi.ToString("0.0")}");
+    }
+
+    /*===========================================*/
+
+}
---
+++ Gabaritos atvs - Domingo/26-06-2022/atividade 3/Program.cs
@@ -0,0 +1,86 @@
+﻿/*
+    Faça um programa que apresente o menu de opções a seguir:
+    Menu de opções:
+    1 - Média aritmética
+    2 - Média ponderada
+    0 - Sair
+    Digite a opção desejada:
+    Na opção 1: receber duas notas, calcular e mostrar a média aritmética.
+    Na opção 2: receber três notas e seus respectivos pesos, calcular e mostrar a média ponderada.
+    Na opção 3: sair do programa.  
+
+    Verifique a possibilidade de opção inválida, mostrando uma mensagem.
+*/
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Que tal fazermos um calculo de média?");
+
+            /*================ Váriaveis ================*/
+
+            Media media = new Media();
+            int num;
+
+            /*===========================================*/
+
+            /*============ Entrada de Dados =============*/
+
+            Console.WriteLine("Digite seu nome:");
+            media.name = Console.ReadLine();
+
+            re:
+            Console.WriteLine("\nQue tipo de media você quer:" +
+                "\n1 - Média aritmética" +
+                "\n2 - Média ponderada" +
+                "\n0 - Sair");
+            num = int.Parse(Console.ReadLine());
+
+            /*===========================================*/
+
+            /*========= Processamento de Dados ==========*/
+
+            if (num == 1)
+            {
+                media.mediaA();
+            }
+            else if (num == 2)
+            {
+                media.mediaP();
+            }
+            else if (num == 0)
+            {
+                
+                goto fin;
+            }
+
+            /*===========================================*/
+
+            /*============= Saída de Dados ==============*/
+
+            media.mensagem();
+
+            Console.WriteLine("\nQuer mais alguma coisa?" +
+                "\n1 - sim" +
+                "\n2 - não");
+            num = int.Parse(Console.ReadLine());
+
+            if (num == 1)
+            {
+                goto re;
+            }
+
+            fin:
+
+            Console.WriteLine("\nObrigado por usar nosso sistema, até logo.");
+
+            /*===========================================*/
+
+            Console.ReadLine();
+        }
+    }
+}
